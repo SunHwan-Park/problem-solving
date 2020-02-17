e4af2b73bbb4cf903c8cd0ef7936519a989041f6
박선환 <witty_man@naver.com>
2020-02-17 17:48:53 +0900

    update_200217_2

--- Swea/1267/1267_2.py
+++ Swea/1267/1267_2.py
@@ -17,87 +17,38 @@ for tc in range(1, 11):
 
     # 각 노드의 방문 여부
     visited = [0 for _ in range(V+1)]
-    answer = ''
 
-    min_accept = 1000000
-    for i in range(1, V+1):
-        if visited[i] != 1: # 방문하지 않은 노드 중에서
-            if sum(t_M[i]) < min_accept: # 해당 노드로 들어오는 노드의 수가 가장 작다면
-                min_accept = sum(t_M[i])
+    # 최종 결과물
+    answer = ''
     
     queue = []
     for i in range(1, V+1):
-        if visited[i] != 1:
-            if sum(t_M[i]) == min_accept:
-                queue.append(i)
-                visited[i] = 1
-                answer += str(i)+' '
-
-    for node in queue:
-        
-    
+        if sum(t_M[i]) == 0:
+            start_queue.append(i)
+            visited[i] = 1
+            answer += str(i)+' '
+                
     while True:
         if sum(visited[1:]) == V and len(queue) == 0:
             break
 
-        for node in queue:
-            visited[node] = 1
-            answer += str(node)+' '
-            for i in range(1, V+1):
-                if M[node][i] == 1 and t_M[i] == min_accept:
-                    queue.append(i)
-            queue.pop()
-        
+        for i in queue:
+            for j in range(1, V+1):
+                if M[i][j] == 1:
+                    
+
+
         min_accept = 1000000
         for i in range(1, V+1):
             if visited[i] != 1: # 방문하지 않은 노드 중에서
                 if sum(t_M[i]) < min_accept: # 해당 노드로 들어오는 노드의 수가 가장 작다면
                     min_accept = sum(t_M[i])
-        
 
-
-
-
-
-    # # firtst_node
-    # first_node = []
-    # for i in range(1, V+1):
-    #     if sum(t_M[i]) == 0:
-    #         first_node.append(i)
-    #         visited[i] = 1
-    # answer = ''
-    
-    
-
-
-    # queue = []
-    # for node in first_node:
-    #     answer += str(node)+' '
-    #     for i in range(1, V+1):
-    #         if M[node][i] == 1 and sum(t_M[i]) == min_accept:
-    #             queue.append(i)
-    #             # visited[i] = 1
-    #             # t_M[i][node] = 0
-
-    # while True:
-    #     if sum(visited[1:]) == V and len(queue) == 0:
-    #         break
-
-    #     min_accept = 1000000
-    #     for i in range(1, V+1):
-    #         if visited[i] != 1:
-    #             if sum(t_M[i]) < min_accept:
-    #                 min_accept = sum(t_M[i])
-
-    #     for node in sorted(queue):
-    #         answer += str(node)+' '
-    #         visited[node] = 1
-    #         for i in range(1, V+1):
-    #             if M[node][i] == 1 and sum(t_M[i]) == min_accept:
-    #                 queue.append(i)
-    #                 # visited[i] = 1
-    #                 # t_M[i][node] = 0
-    #         queue.pop(0)
-
+        for i in range(1, V+1):
+            if visited[i] != 1:
+                if sum(t_M[i]) == min_accept:
+                    stack.append(i)
+                    visited[i] = 1
+                    answer += str(i)+' '
     
     print('#{} {}'.format(tc, answer))
--- Swea/1873/1873.py
+++ Swea/1873/1873.py
@@ -31,75 +31,63 @@ for tc in range(1, T+1):
         if command[i] == 'U':
             d = 'up'
             M[loc[0]][loc[1]] = '^'
-            try:
+            if loc[0] - 1 >= 0:
                 if M[loc[0]-1][loc[1]] == '.':
                     M[loc[0]][loc[1]], M[loc[0]-1][loc[1]] = M[loc[0]-1][loc[1]], M[loc[0]][loc[1]]
                     loc[0] -= 1
-            except:
-                pass
         elif command[i] == 'D':
             d = 'down'
             M[loc[0]][loc[1]] = 'v'
-            try:
+            if loc[0] + 1 < H:
                 if M[loc[0]+1][loc[1]] == '.':
                     M[loc[0]][loc[1]], M[loc[0]+1][loc[1]] = M[loc[0]+1][loc[1]], M[loc[0]][loc[1]]
                     loc[0] += 1
-            except:
-                pass
         elif command[i] == 'L':
             d = 'left'
             M[loc[0]][loc[1]] = '<'
-            try:
+            if loc[1] - 1 >= 0:
                 if M[loc[0]][loc[1]-1] == '.':
                     M[loc[0]][loc[1]], M[loc[0]][loc[1]-1] = M[loc[0]][loc[1]-1], M[loc[0]][loc[1]]
                     loc[1] -= 1
-            except:
-                pass
         elif command[i] == 'R':
             d = 'right'
             M[loc[0]][loc[1]] = '>'
-            try:
+            if loc[1] + 1 < W:
                 if M[loc[0]][loc[1]+1] == '.':
                     M[loc[0]][loc[1]], M[loc[0]][loc[1]+1] = M[loc[0]][loc[1]+1], M[loc[0]][loc[1]]
                     loc[1] += 1
-            except:
-                pass
         elif command[i] == 'S':
             shoot_loc = loc[:]
             if d == 'up':
                 while M[shoot_loc[0]][shoot_loc[1]] not in ['*', '#']:
-                    try:
-                        M[shoot_loc[0]-1][shoot_loc[1]] = M[shoot_loc[0]-1][shoot_loc[1]]
-                    except:
+                    if shoot_loc[0]-1 >= 0:
+                        shoot_loc[0] -= 1
+                    else:
                         break
-                    shoot_loc[0] -= 1
                 if M[shoot_loc[0]][shoot_loc[1]] == '*':
                     M[shoot_loc[0]][shoot_loc[1]] = '.'
             elif d == 'down':
                 while M[shoot_loc[0]][shoot_loc[1]] not in ['*', '#']:
-                    try:
-                        M[shoot_loc[0]+1][shoot_loc[1]] = M[shoot_loc[0]+1][shoot_loc[1]]
-                    except:
+                    if shoot_loc[0]+1 < H:
+                        shoot_loc[0] += 1
+                    else:
                         break
-                    shoot_loc[0] += 1
                 if M[shoot_loc[0]][shoot_loc[1]] == '*':
                     M[shoot_loc[0]][shoot_loc[1]] = '.'
             elif d == 'left':
                 while M[shoot_loc[0]][shoot_loc[1]] not in ['*', '#']:
-                    try:
-                        M[shoot_loc[0]][shoot_loc[1]-1] = M[shoot_loc[0]][shoot_loc[1]-1]
-                    except:
-                        break
-                    shoot_loc[1] -= 1
+                    if shoot_loc[1]-1 >= 0:
+                        shoot_loc[1] -= 1
+                    else:
+                        break                    
                 if M[shoot_loc[0]][shoot_loc[1]] == '*':
                     M[shoot_loc[0]][shoot_loc[1]] = '.'
             elif d == 'right':
                 while M[shoot_loc[0]][shoot_loc[1]] not in ['*', '#']:
-                    try:
-                        M[shoot_loc[0]][shoot_loc[1]+1] = M[shoot_loc[0]][shoot_loc[1]+1]
-                    except:
-                        break
-                    shoot_loc[1] += 1
+                    if shoot_loc[1]+1 < W:
+                        shoot_loc[1] += 1
+                    else:
+                        break                    
                 if M[shoot_loc[0]][shoot_loc[1]] == '*':
                     M[shoot_loc[0]][shoot_loc[1]] = '.'
     
@@ -107,4 +95,58 @@ for tc in range(1, T+1):
     result = ''
     for i in range(H):
         result += ''.join(M[i])+'\n'
-    print('#{} {}'.format(tc, result))
+    print('#{} {}'.format(tc, result.strip()))
+
+
+# ----------------------------------------------
+# Dev.J 코드
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
+ 
+def shoot(i, j, d):
+    y, x = i, j
+    while True:
+        y += di[d]
+        x += dj[d]
+        if 0 <= x < W and 0 <= y < H:
+            if arr[y][x] == '#':
+                return
+            elif arr[y][x] == '*':
+                arr[y][x] = '.'
+                return
+        else:
+            return
+ 
+T = int(input())
+for t in range(1, T+1):
+    H, W = map(int, input().split())
+    arr = [list(map(str, input())) for _ in range(H)]
+    N = int(input())
+    C = input() #Command
+ 
+    tank = '^>v<'
+    tank_dir = {'^': 0, '>': 1, 'v': 2, '<': 3}
+    for i in range(H):
+        for j in range(W):
+            if arr[i][j] in tank:
+                tank_idx = [i, j, tank_dir[arr[i][j]]]
+                arr[i][j] = '.'
+ 
+    move = 'URDL' #방향을 해당 방향으로 바꾸고, 전진 가능하면 전진
+    for i in range(N):
+        if C[i] == 'S':
+            shoot(tank_idx[0], tank_idx[1], tank_idx[2])
+        else:
+            for j in range(len(move)):
+                if C[i] == move[j]:
+                    my = tank_idx[0]+di[j]
+                    mx = tank_idx[1]+dj[j]
+                    if 0 <= my < H and 0 <= mx < W:
+                        if arr[my][mx] == '.':
+                            tank_idx[0] += di[j]
+                            tank_idx[1] += dj[j]
+                    tank_idx[2] = j
+    arr[tank_idx[0]][tank_idx[1]] = tank[tank_idx[2]]
+    print("#"+str(t), end=" ")
+    for i in range(len(arr)):
+        print(''.join(arr[i]))
