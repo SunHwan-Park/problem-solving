c0c78c65d7daab6ce567f2fde5e43e0932923ce1
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-02-27 21:09:35 +0900

    update_200227_1

---
+++ Swea/1222/1222.py
@@ -0,0 +1,54 @@
+isp = {'*':2, '/':2, '+':1, '-':1, '(':0}
+icp = {'*':2, '/':2, '+':1, '-':1, '(':3}
+
+def to_b(D):
+    stack = []
+    result = ''
+    for d in D:
+        try:
+            if d in isp.keys():
+                if isp[stack[-1]] >= icp[d]:
+                    while isp[stack[-1]] >= icp[d]:
+                        result += stack.pop()
+                    stack.append(d)
+                else:
+                    stack.append(d)
+            elif d == ')':
+                while stack[-1] != '(':
+                    result += stack.pop()
+                stack.pop()
+            else:
+                result += d
+        except:
+            stack.append(d)
+    
+    if stack:
+        result += stack.pop()
+    
+    return result
+
+def to_m(D):
+    stack = []
+    for d in D:
+        if d in isp.keys():
+            if d == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif d == '-':
+                stack.append(stack.pop() - stack.pop())
+            elif d == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif d == '/':
+                stack.append(stack.pop() / stack.pop())
+        else:
+            stack.append(int(d))
+    return stack[0]
+
+import sys
+sys.stdin = open('input.txt')
+
+T = 10
+for tc in range(1, T+1):
+    N = int(input())
+    D = input()
+
+    print('#{} {}'.format(tc, to_m(to_b(D))))
--- Swea/1258/1258.py
+++ Swea/1258/1258.py
@@ -1,42 +1,42 @@
-import sys
-sys.stdin = open('input.txt')
+# import sys
+# sys.stdin = open('input.txt')
 
-T = int(input())
-for tc in range(1, T+1):
-    n = int(input())
-    matrix = []
-    for _ in range(n):
-        matrix.append(''.join(input().split()).split('0'))
-    for i in range(n):
-        for j in range(n):
-                if matrix[i][j] != '':
-                    for k in range(len(matrix[i][j])):
-                        matrix[i].insert(j+1, '')
+# T = int(input())
+# for tc in range(1, T+1):
+#     n = int(input())
+#     matrix = []
+#     for _ in range(n):
+#         matrix.append(''.join(input().split()).split('0'))
+#     for i in range(n):
+#         for j in range(n):
+#                 if matrix[i][j] != '':
+#                     for k in range(len(matrix[i][j])):
+#                         matrix[i].insert(j+1, '')
 
-    result = []
-    for i in range(n):
-        for j in range(n):
-            try:
-                if matrix[i][j] != '':
-                    col = len(matrix[i][j])
-                    row = 1
-                    k = i+1
-                    matrix[i].remove(matrix[i][j])
-                    matrix[i].insert(j, '')
-                    while matrix[k][j] != '' and k != n:
-                        matrix[k].remove(matrix[k][j])
-                        matrix[k].insert(j, '')
-                        k += 1
-                        row += 1
-                    result.append([row, col])
-            except:
-                pass
+#     result = []
+#     for i in range(n):
+#         for j in range(n):
+#             try:
+#                 if matrix[i][j] != '':
+#                     col = len(matrix[i][j])
+#                     row = 1
+#                     k = i+1
+#                     matrix[i].remove(matrix[i][j])
+#                     matrix[i].insert(j, '')
+#                     while matrix[k][j] != '' and k != n:
+#                         matrix[k].remove(matrix[k][j])
+#                         matrix[k].insert(j, '')
+#                         k += 1
+#                         row += 1
+#                     result.append([row, col])
+#             except:
+#                 pass
     
-    answer = ''
-    for square in sorted(sorted(result), key=lambda x: x[0]*x[1]):
-        answer += '{} {} '.format(square[0], square[1])
+#     answer = ''
+#     for square in sorted(sorted(result), key=lambda x: x[0]*x[1]):
+#         answer += '{} {} '.format(square[0], square[1])
 
-    print('#{} {} {}'.format(tc, len(result), answer))
+#     print('#{} {} {}'.format(tc, len(result), answer))
 
 # --------------------------------------------------
 import sys
