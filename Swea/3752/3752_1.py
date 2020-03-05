fb226a6080642a52e9db6ba1eca44201d3846e9d
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-03-05 16:46:07 +0900

    update_200305_1

---
+++ Swea/3752/3752_1.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
+from itertools import combinations
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    D = list(map(int, input().split()))
+    R = set()
+    for i in range(N//2+1):
+        for c in combinations(D, i):
+            R.add(sum(c))
+            temp = D[:]
+            for k in c:
+                temp.remove(k)
+            R.add(sum(temp))
+    print('#{} {}'.format(tc, len(R)))
