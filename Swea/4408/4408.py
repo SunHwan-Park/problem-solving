289efc55bb4d9ae6a50e1dc3d04e22e61cf683e4
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-03-10 23:05:34 +0900

    update_200310_1

---
+++ Swea/4408/4408.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    D = [list(map(lambda x: (x+1)//2, map(int, input().split()))) for _ in range(N)]
+    V = [0]*201
+    for d in D:
+        if d[0] <= d[1]:
+            a = d[0]
+            b = d[1]
+        for i in range(a, b+1):
+            V[i] += 1
+    print('#{} {}'.format(tc, max(V)))
