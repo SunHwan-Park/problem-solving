69727a7e1824ef7ffd708214a9c22fbea34f18f2
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-03-28 21:36:06 +0900

    update_200328_1

--- Swea/4871/4871.py
+++ Swea/4871/4871.py
@@ -1,6 +1,32 @@
 import sys
 sys.stdin = open('input.txt')
 
+def dfs(node, V, k):
+    global G
+    global result
+    if node == G:
+        result = 1
+        return
+    elif k == V:
+        return
+    else:
+        for i in range(V+1):
+            if M[node][i] == 1 and visit[i] == 0:
+                visit[i] = 1
+                dfs(i, V, k+1)
+                visit[i] = 0
+
 T = int(input())
 for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    M = [[0]*(V+1) for _ in range(V+1)]
+    for _ in range(E):
+        start, end = map(int, input().split())
+        M[start][end] = 1
+    S, G = map(int, input().split())
+    visit = [0]*(V+1)
+    visit[S] = 1
+    result = 0
+    dfs(S, V, 1)
     
+    print('#{} {}'.format(tc, result))
--- Swea/5688/5688.py
+++ Swea/5688/5688.py
@@ -3,4 +3,10 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 for tc in range(1, T+1):
-    
+    N = int(input())
+    temp = round(N**(1/3))
+    result = -1
+    for i in [-1, 0, 1]:
+        if (temp+i)**3 == N and temp+i > 0:
+            result = temp+i
+    print('#{} {}'.format(tc, result))
