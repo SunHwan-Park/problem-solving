cfc3e53bfc54d33fac2c3351b4784de670d5b794
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-04-30 23:51:59 +0900

    solve swea 1242

--- swea/1242/1242.py
+++ swea/1242/1242.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('input2.txt')
+sys.stdin = open('input.txt')
 
 # 0. 암호코드 정보 추출을 위한 dictionary 생성
 password_code = {'0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '0100011': 4, '0110001': 5, '0101111': 6, '0111011': 7, '0110111' : 8, '0001011': 9}
@@ -8,38 +8,56 @@ password_code = {'0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '010001
 T = int(input())
 for tc in range(1, T+1):
     N, M = list(map(int, input().split()))
-    final_result = 0
-
+    answer = 0
+    possible = set()
     # 2. 암호코드 추출
+    lines = set()
     for _ in range(N):
-        line = bin(int(input(), 16))[2:]
-        for i in range(len(line)-1, -1, -1): # 들어오는 배열마다, 뒤에서 부터 '1' 탐색
-            if line[i] == '1':
-                result = []
+        line = bin(int(input(), 16))[2:].strip('0')
+        if line not in lines:
+            lines.add(line)
+            while line:    
                 k = 1
-                while len(result) < 8:
-                    if i+1-56*k >= 0:
-                        code = line[i+1-56*k:i+1] # 56자리 암호코드 추출
-                    else:
-                        code = line[:i+1]
-                    if len(code) % 56:
-                        code = ((len(code)//56 + 1)*56 - len(code))*'0' + code
-                    for j in range(8):
-                        try:
-                            temp = code[7*j:7*(j+1)]
-                            revised_code = ''
-                            for l in range(0, k*7, k):
-                                revised_code += temp[l]
-                            result.append(password_code[revised_code])
-                        except:
-                            result = []
-                            k += 1 
-                            break
-                validation = result[-1] # 검증코드 먼저 더하기
-                for idx in range(7):
-                    if idx % 2:
-                        validation += result[idx] # 코드의 순서가 짝수 일때
-                    else:
-                        validation += result[idx] * 3 # 코드의 순서가 홀수 일때
-                if validation % 10 == 0:
-                    final_result += sum(result)
+                while True:
+                    endcode1 = line[-7*k:]
+                    endcode2 = line[-7*2*k:-7*k]
+                    revised_endcode1 = ''
+                    revised_endcode2 = ''
+                    for i in range(0, k*7, k):
+                        revised_endcode1 += endcode1[i]
+                        revised_endcode2 += endcode2[i]
+                    try:
+                        password_code[revised_endcode1]
+                        password_code[revised_endcode2]
+                        if len(line) >= 56*k:
+                            possible.add(line[-56*k:].strip('0'))
+                            line = line[:-56*k].strip('0')
+                            k = 1
+                        else:
+                            possible.add(line.strip('0'))
+                            line = ''
+                        break
+                    except:
+                        k += 1
+
+    for code in possible:
+        if len(code) % 56:
+            code = ((len(code)//56 + 1)*56 - len(code))*'0' + code
+        result = []
+        k = len(code)//56
+        for i in range(8):
+            revised_code = ''
+            temp = code[7*i*k:7*(i+1)*k]
+            for j in range(0, k*7, k):
+                revised_code += temp[j]
+            result.append(password_code[revised_code])
+        validation = result[-1] # 검증코드 먼저 더하기
+        for idx in range(7):
+            if idx % 2:
+                validation += result[idx] # 코드의 순서가 짝수 일때
+            else:
+                validation += result[idx] * 3 # 코드의 순서가 홀수 일때
+        if validation % 10 == 0:
+            answer += sum(result)
+    
+    print('#{} {}'.format(tc, answer))
